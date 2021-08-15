39a58b076bd6f543cdc2759afc9689124c08d43d
Kevin Hanselman <kevin.hanselman@protonmail.com>
2021-08-15 13:14:08 -0400

    add basic benchmark suite

---
+++ bench_test.go
@@ -0,0 +1,48 @@
+package yaml
+
+import (
+	"testing"
+)
+
+type benchStruct struct {
+	a int
+	b string
+	c map[string]float32
+}
+
+func BenchmarkMarshal(b *testing.B) {
+	s := benchStruct {
+		a: 5,
+		b: "foobar",
+		c: map[string]float32{
+			"bish": 1.2,
+			"bash": 3.4,
+			"bosh": 5.6,
+		},
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		if _, err := Marshal(s); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkUnmarshal(b *testing.B) {
+	yaml := []byte(`a: 5
+b: "foobar"
+c:
+  bish: 1.2
+  bash: 3.4
+  bosh: 5.6
+`)
+	var s benchStruct
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		if err := Unmarshal(yaml, &s); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
