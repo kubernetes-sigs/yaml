53ce8bbaa29cd2c835bc0a96e6a849774442d67b
Arthur Diniz <arthurbdiniz@gmail.com>
2025-06-17 20:24:16 +0100

    Fix int64 minimum value handling in tests for cross-architecture compatibility
    Update test cases that use the int64 minimum value
    (-9223372036854775808) to avoid overflow errors on
    32-bit (i386) architectures.
    
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- goyaml.v3/decode_test.go
+++ goyaml.v3/decode_test.go
@@ -177,7 +177,7 @@ var unmarshalTests = []struct {
 		map[string]interface{}{"bin": -42},
 	}, {
 		"bin: -0b1000000000000000000000000000000000000000000000000000000000000000",
-		map[string]interface{}{"bin": -9223372036854775808},
+		map[string]int64{"bin": -9223372036854775808},
 	}, {
 		"decimal: +685_230",
 		map[string]int{"decimal": 685230},
